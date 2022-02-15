de1a56bb844c6c8fa0ff20ac75a9bf60da1c893e
CodeCulture <50676659+priyansh1997@users.noreply.github.com>
2022-02-15 16:24:40 +0530

    changed desc

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db=SQLAlchemy(app)
 class Todo(db.Model):
     sno=db.Column(db.Integer(), primary_key=True)
     title=db.Column(db.String(200), nullable=False)
-    desc=db.Column(db.String(1000))
+    desc=db.Column(db.String(1000), nullable=False)
     date_created=db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
